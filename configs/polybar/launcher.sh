5ff5e1fa7aa5f030f7ce183797017ca07676cb65
Nelio Santos <nsfilho@icloud.com>
2024-01-29 13:41:52 -0300

    feat(core): i3 changes

--- configs/polybar/launcher.sh
+++ configs/polybar/launcher.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 
 # Terminate already running bar instances
-# If all your bars have ipc enabled, you can use 
+# If all your bars have ipc enabled, you can use
 polybar-msg cmd quit
 # Otherwise you can use the nuclear option:
 # killall -q polybar
 
+# delay for detect monitors properly
+sleep 3
+
 # launch polybar on all monitors
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar --reload mybar &
+	echo "Launching polybar on monitor $m"
+	MONITOR=$m polybar --reload mybar &
 done
