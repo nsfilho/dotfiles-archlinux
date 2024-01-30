1bf008d04481dcb8422e89ace1cf92058943824d
Nelio Santos <nsfilho@icloud.com>
2024-01-30 10:42:08 -0300

    feat: local editing

--- setup-dev.sh
+++ setup-dev.sh
@@ -10,3 +10,14 @@ echo Installing deno
 if [ ! -f ~/.deno/bin/deno ]; then
 	curl -fsSL https://deno.land/x/install/install.sh | sh
 fi
+
+#
+# -- exists package in arch repo
+#
+# echo "📦 Setup docker environment"
+# if [ ! -x /usr/bin/docker ]; then
+# 	echo Installing docker
+# 	curl -fsSL https://get.docker.com -o /tmp/get-docker.sh
+# 	sudo sh /tmp/get-docker.sh
+# 	sudo usermod -aG docker $USER
+# fi
